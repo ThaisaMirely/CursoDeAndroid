11c5459cb0236ba7bc57b8fff84b3da02ca87795
ThaisaMirely <mirelythaisa@gmail.com>
2015-08-22 09:10:13 -0300

    teste
    testando

---
+++ AcademiaJedi/Aula2-BackEnd_FrontEnd/app/src/main/java/tmbs/aula2_backend_frontend/componente/Dialog.java
@@ -0,0 +1,25 @@
+package tmbs.aula2_backend_frontend.componente;
+
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by thaisamirely on 8/22/15.
+ */
+public class Dialog {
+    private AlertDialog alertDialog;
+    public void BasicDialog(){
+
+    }
+
+    public void CustomDialog(){
+
+
+    }
+
+    public void MultiListDialog(){
+
+
+
+
+    }
+}
